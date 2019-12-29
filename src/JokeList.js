9d02d227421785c1a07044dee2e8dca02d1981b8
mhouston4994 <mhouston4994@gmail.com>
2019-12-28 21:27:59 -0700

    refactored axios call, added styling, added logic for popularity

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './JokeList.css';
+import uuid from 'uuid/v4';
 import Joke from './Joke';
 
 class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    };
     constructor(props){
         super(props);
         this.state = {
-            jokes: [1,2,3],
+            jokes: [],
         }
 
     }
-    render(){
-        //Create GET Request
-        const pullRequest = async run => {
-            let data = await axios.get('https://icanhazdadjoke.com/', {
-                headers: {
+
+    async componentDidMount() {
+        //Load Jokes
+        let jokes = [];
+        while(jokes.length < this.props.numJokesToGet){
+            let res = await axios.get('https://icanhazdadjoke.com/', {
+            headers: {
                     Accept: 'application/json'
                 }
-            })
-                .then(response => {
-                    let jokes = response.data.joke;
-                        <Joke theJoke={jokes} />
-                })
+            });
+            jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
         }
-        
-        const allJokes = this.state.jokes.map(pullRequest);
+        this.setState({ jokes: jokes });
+    }
+    handleVote(id, delta){
+        this.setState(
+            st => ({
+                jokes: st.jokes.map(j =>
+                j.id === id ? { ...j, votes: j.votes + delta} : j)
+            })
+        )
+    }
+    render(){
         return(
             <div className="JokeList">
-                {allJokes}
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
+                    <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+                    <button className="JokeList-getmore">More Jokes</button>
+                </div>
+                <div className="JokeList-jokes">
+                    {this.state.jokes.map(j => (
+                        <Joke 
+                        key={j.id} 
+                        votes={j.votes} 
+                        text={j.text} 
+                        upvote={() => this.handleVote(j.id, 1)}
+                        downvote={() => this.handleVote(j.id, -1)}
+                        />
+                    ))}
+                </div>
             </div>
         )
     }
